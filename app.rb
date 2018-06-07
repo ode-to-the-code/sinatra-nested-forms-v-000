604823ecce08a7e048d967571465025a14b92d45
ode-to-the-code <justcallmethis@gmail.com>
2018-06-07 10:43:30 +0700

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,17 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = params
+    end
+
   end
 end
